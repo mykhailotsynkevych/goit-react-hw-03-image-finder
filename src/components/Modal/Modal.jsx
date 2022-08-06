db0be4d6a476af6d8afbdaa74e99ebebc1369e00
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-08-06 21:22:53 +0300

    Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,21 +1,39 @@
 import { createPortal } from 'react-dom';
+import { Component } from "react";
 
 const modalContainer = document.getElementById('modal');
 
-const Modal = ({ modalFoto, setModalFoto }) => {
-  const handleModalClose = e => {
-    if (e.target === e.currentTarget) return;
-    setModalFoto();
+
+class Modal extends Component {
+
+    componentDidMount() {
+    window.addEventListener("keydown", this.handleModalClose);
+  }
+
+  //   componentWillUnmount() {
+  //   window.removeEventListener("keydown", this.handleModalClose);
+  // }
+
+  handleModalClose = (e) => {
+    console.log("Modal");
+    if (e.target !== e.currentTarget || e.code === "Escape"){
+      this.props.setModalFoto();
+}
+
   };
 
+  render() {
+    const { modalFoto } = this.props;
+
   return createPortal(
-    <div className="Overlay" onClick={handleModalClose}>
+    <div className="Overlay" onClick={this.handleModalClose}>
       <div className="Modal">
         <img src={modalFoto} alt="" />
       </div>
     </div>,
     modalContainer
   );
-};
+  }
+}
 
 export default Modal;
